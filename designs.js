830c3132fd52b9886adfc441773ddad7cc6bf10d
Desiree Foard <foard.desiree@gmail.com>
2017-09-16 09:53:40 -0600

    added variables for colorInput, colorPicker, and Submit button

--- designs.js
+++ designs.js
@@ -1,6 +1,8 @@
 // Select color input
+let colorInput = $('#colorPicker');
+let choosenColor = $('#colorPicker').val();
 // Select size input
-
+let submit = $('input submit');
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
